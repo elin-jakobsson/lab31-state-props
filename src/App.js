aabe063bd47de5ab80ae808cc0b138246e4f62af
Albin Engman <albin.a.engman@gmail.com>
2018-04-25 16:06:14 +0200

    Finished Login and LockedText

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import Login from './Login.js'
+import LockedText from './LockedText'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        Hello World 
+        Hello World
+        <Login></Login>
+        <LockedText></LockedText>
       </div>
     );
   }
--- src/LockedText.js
+++ src/LockedText.js
@@ -1,8 +1,33 @@
 import React from 'react';
 
 class LockedText extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      checked: false,
+      disabled: true,
+    }
+
+    this.checkFunction = this.checkFunction.bind(this);
+  }
+  checkFunction(event){
+    this.setState({checked: !this.state.checked});
+  }
   render() {
+    if (this.state.checked === true){
+      this.state.disabled = false;
+    }
+    else if (this.state.checked === false){
+      this.state.disabled = true;
+    }
     return (
+      <div className = "lockedTextContainer">
+      <div className = "checkContainer">
+        <input type = "checkbox" id = "textCheck" onClick = {this.checkFunction}/>
+        <label htmlFor = "textCheck">Enable textarea</label>
+      </div>
+      <textarea placeholder = "Only writable when checked!" disabled = {this.state.disabled} ></textarea>
+      </div>
     );
   }
 }
