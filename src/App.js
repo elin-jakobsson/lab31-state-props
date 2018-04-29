1ffef08b37ed181504e4dcbcc69916dd3ddabf62
Elin Jakobsson <elin-jakobsson@hotmail.com>
2018-04-29 15:08:37 +0200

    tabs for all components

--- src/App.js
+++ src/App.js
@@ -3,16 +3,18 @@ import logo from './logo.svg';
 import Login from './Login.js'
 import LockedText from './LockedText'
 import BackwardsText from './BackwardsText'
+import Tabs from './Tabs'
 import './App.css';
-import GuessNumber from './GuessNumber'
 
-const randomNumberGenerator =Math.round( (Math.random()*10)+1);
+
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         Hello World
-        <GuessNumber guess={randomNumberGenerator}/>
+        <Tabs/>
+
         <Login/>
         <LockedText/>
         <BackwardsText/>
--- src/Tabs.js
+++ src/Tabs.js
@@ -1,9 +1,72 @@
 import React from 'react';
+import GuessNumber from './GuessNumber'
+import Login from './Login.js'
+import LockedText from './LockedText'
+import BackwardsText from './BackwardsText'
+
+import './css/tabs.css'
+
+
+const randomNumberGenerator =Math.round( (Math.random()*100)+1);
 
 class Tabs extends React.Component {
+  constructor(props){
+    super(props);
+    this.state={
+      tabPage:'hem'
+    }
+    this.homeTab = this.homeTab.bind(this);
+    this.lockTab = this.lockTab.bind(this);
+    this.loginTab = this.loginTab.bind(this);
+    this.backTextTab = this.backTextTab.bind(this);
+    this.guessTab = this.guessTab.bind(this);
+
+  }
+  homeTab(event){
+      this.setState({tabPage:'hem'});
+  }
+  lockTab(event){
+    this.setState({tabPage:'lockt'});
+  }
+  loginTab(event){
+    this.setState({tabPage:'login'});
+  }
+  backTextTab(event){
+    this.setState({tabPage:'backText'});
+  }
+  guessTab(event){
+    this.setState({tabPage:'guessNum'});
+  }
   render() {
+
+    const currentTab =(this.state.tabPage==='hem') ?
+      <div>
+        <h1>Välkommen till lab 31</h1>
+        <p>Denna laboration består av övningar i react.js</p>
+      </div>
+    :(this.state.tabPage==='login') ?
+      <Login/>
+    :(this.state.tabPage==='lockt') ?
+      <LockedText/>
+    :(this.state.tabPage==='backText') ?
+      <BackwardsText/>
+    :(this.state.tabPage==='guessNum') ?
+    <GuessNumber guess={randomNumberGenerator}/>
+    : 'hem'
+
     return (
-      //hi
+      <div>
+        <div className='tab-button-container'>
+        <button onClick={this.homeTab}>Hem</button>
+        <button onClick={this.loginTab} >Logga in</button>
+        <button onClick={this.lockTab} >Låst textfält</button>
+        <button onClick={this.backTextTab} >Baklängestext</button>
+        <button onClick={this.guessTab}>Gissa talet</button>
+        </div>
+        <div className='current-tab-container'>
+        {currentTab}
+        </div>
+      </div>
     );
   }
 }
